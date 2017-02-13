2c68ff82d53d7e04f49373e5d12e558f81796582
ludvigk <ludde.killingberg@gmail.com>
2017-02-13 15:42:00 +0100

    Added staticfiles and url linking

--- mysite/login/views.py
+++ mysite/login/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Context, loader
 
-# Create your views here.
+def index(request):
+    return render(request, 'login/index.html')
