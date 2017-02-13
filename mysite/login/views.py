b4c125f5839626b315ce79773212c32fe2136136
ludvigk <ludde.killingberg@gmail.com>
2017-02-13 15:55:34 +0100

    Fixed pycodestyle error

--- mysite/login/views.py
+++ mysite/login/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Context, loader
 
+
 def index(request):
     return render(request, 'login/index.html')
