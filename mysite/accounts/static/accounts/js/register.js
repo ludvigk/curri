6c3488957f8415c282e1e07aa0d28ec3e5c6ef48
Henrik Munkeberg <exburn0@gmail.com>
2017-02-15 16:03:09 +0100

    register.js

---
+++ mysite/accounts/static/accounts/js/register.js
@@ -0,0 +1,13 @@
+function register() {
+  $.get('/accounts/register/', {username: $('#username').val(), email: $('#email').val(), password: $('#password').val()},
+  function(data){
+
+    if(data == "True"){
+      //Redirect til register success
+      window.location.replace("/");
+
+    }
+  
+  });
+}
+$('#username').change(function (){CheckUsername()});
