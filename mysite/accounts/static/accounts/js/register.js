8197a86ed388469056fd48781c6bf04a5951d657
ludvigk <ludde.killingberg@gmail.com>
2017-02-20 13:20:45 +0100

    Fixed registration button and added basics of email verification

--- mysite/accounts/static/accounts/js/register.js
+++ mysite/accounts/static/accounts/js/register.js
@@ -1,14 +1,30 @@
 function register() {
-  console.log("hello");
-  $.get('/accounts/register/', {username: $('#username').val(), email: $('#email').val(), password: $('#password').val(), confirm_password: $('#confirm-password').val()},
-  function(data){
-    if(data == "True"){
-      //Redirect til register success
-      window.location.replace("/");
+	var name = $("#name").val();
+	var email = $("#email").val();
+	var password = $("#password").val();
+	var cpassword = $("#confirm-password").val();
 
-    } else {
-      //
-    }
-  });
+	if (name == '' || email == '' || password == '' || cpassword == '') {
+		alert("Fyll ut alle felter");
+	} 
+	else if ((password.length) < 8) {
+		alert("Passord må være minst 8 karakterer langt");
+	} 
+	else if (!(password).match(cpassword)) {
+		alert("Passord matcher ikke");
+	} 
+	else {
+		$.get('/accounts/register/', {username: $('#username').val(), email: $('#email').val(), password: $('#password').val()},
+  		function(data){
+
+    		if(data == "True"){
+      		//Redirect til register success
+      		window.location.replace("/");
+
+    		}
+  
+   		});
+	}
+
 }
 $('#register-submit').click(function (){register()});
