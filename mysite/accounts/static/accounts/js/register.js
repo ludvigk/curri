21181ef54c28033567903436470bfa1067669eb7
Henrik Munkeberg <exburn0@gmail.com>
2017-02-20 13:02:51 +0100

    register validation v1

--- mysite/accounts/static/accounts/js/register.js
+++ mysite/accounts/static/accounts/js/register.js
@@ -1,13 +1,30 @@
 function register() {
-  $.get('/accounts/register/', {username: $('#username').val(), email: $('#email').val(), password: $('#password').val()},
-  function(data){
+	var name = $("#name").val();
+	var email = $("#email").val();
+	var password = $("#password").val();
+	var cpassword = $("#confirm-password").val();
 
-    if(data == "True"){
-      //Redirect til register success
-      window.location.replace("/");
+	if (name == '' || email == '' || password == '' || cpassword == '') {
+		alert("Fyll ut alle felter");
+	} 
+	else if ((password.length) < 8) {
+		alert("Passord må være minst 8 karakterer langt");
+	} 
+	else if (!(password).match(cpassword)) {
+		alert("Passord matcher ikke");
+	} 
+	else {
+		$.get('/accounts/register/', {username: $('#username').val(), email: $('#email').val(), password: $('#password').val()},
+  		function(data){
 
-    }
+    		if(data == "True"){
+      		//Redirect til register success
+      		window.location.replace("/");
+
+    		}
   
-  });
+   		});
+	}
+
 }
 $('#register-submit').click(function (){register()});
