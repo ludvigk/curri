d1add568f5eba484790c96f49ae9da6f4e28e0f4
Henrik Munkeberg <exburn0@gmail.com>
2017-02-20 13:30:20 +0100

    Register fix

--- mysite/accounts/static/accounts/js/register.js
+++ mysite/accounts/static/accounts/js/register.js
@@ -1,14 +1,20 @@
 function register() {
-	var name = $("#name").val();
+	var name = $("#username").val();
 	var email = $("#email").val();
 	var password = $("#password").val();
 	var cpassword = $("#confirm-password").val();
 
 	if (name == '' || email == '' || password == '' || cpassword == '') {
 		alert("Fyll ut alle felter");
-	} 
-	else if ((password.length) < 8) {
-		alert("Passord må være minst 8 karakterer langt");
+	}
+	else if(/^[a-zA-Z0-9]*$/.test(name) == false) {
+	    alert('Your search string contains illegal characters');
+	}
+	else if (name.length < 6 || name.length > 20) {
+		alert("Brukernavnet må være på mellom 6 og 20 tegn")
+	}
+	else if ((password.length) < 8 || name.length > 32) {
+		alert("Passord må være mellom 8-32 karakterer langt");
 	} 
 	else if (!(password).match(cpassword)) {
 		alert("Passord matcher ikke");
