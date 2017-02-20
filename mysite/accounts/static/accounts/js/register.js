93ae87315de0fc10724a4397e2d4222b626ed30c
ludvigk <ludde.killingberg@gmail.com>
2017-02-20 13:06:20 +0100

    Fixed register button, added basics of email verification

--- mysite/accounts/static/accounts/js/register.js
+++ mysite/accounts/static/accounts/js/register.js
@@ -1,13 +1,14 @@
 function register() {
-  $.get('/accounts/register/', {username: $('#username').val(), email: $('#email').val(), password: $('#password').val()},
+  console.log("hello");
+  $.get('/accounts/register/', {username: $('#username').val(), email: $('#email').val(), password: $('#password').val(), confirm_password: $('#confirm-password').val()},
   function(data){
-
     if(data == "True"){
       //Redirect til register success
       window.location.replace("/");
 
+    } else {
+      //
     }
-  
   });
 }
 $('#register-submit').click(function (){register()});
