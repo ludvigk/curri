a45c8ad5252be0ad335282fc22ac719b82e42cbc
Erlend Fauchald <erlenfau@stud.ntnu.no>
2017-02-20 13:39:26 +0100

    Added UI for password comparison

---
+++ mysite/accounts/static/accounts/js/checkconfirmpassword.js
@@ -0,0 +1,22 @@
+function CheckPassword() {
+    var password1 = document.getElementById("password").value;
+    var password2 = document.getElementById("confirm-password").value;
+    
+    
+    if (password1 == password2){
+        document.getElementById("password").style.backgroundColor = "#90EE90";
+        document.getElementById("confirm-password").style.backgroundColor = "#90EE90";
+
+    }
+    else if ((password1=='')||(password2==''))  {        document.getElementById("password").style.backgroundColor = "#FFFFFF";
+    document.getElementById("confirm-password").style.backgroundColor = "#FFFFFF";
+
+    }
+    else {
+        document.getElementById("password").style.backgroundColor = "#f08080";
+        document.getElementById("confirm-password").style.backgroundColor = "#f08080";
+
+    }    
+      
+    }
+$('#password, #confirm-password').change(function (){CheckPassword()});
