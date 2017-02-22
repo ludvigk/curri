3c988e7a6b08dde71a3263498e2a2c8f17d8cac8
Henrik Munkeberg <exburn0@gmail.com>
2017-02-22 17:13:03 +0100

    Deilig validering og tilbakemelding i registreringen

--- mysite/accounts/static/accounts/js/register.js
+++ mysite/accounts/static/accounts/js/register.js
@@ -4,25 +4,63 @@ function register() {
 	var password = $("#password").val();
 	var cpassword = $("#confirm-password").val();
 
+	var toReset = [false,false,false]
+
+	var should_register = true
+
 	if (name == '' || email == '' || password == '' || cpassword == '') {
-		alert("Fyll ut alle felter");
+
+	    document.getElementById("username-error").innerHTML = "All fields must be filled out"
+	    $('#collapse-username').collapse('show')
+		should_register = false
 	}
 	else if(/^[a-zA-Z0-9]*$/.test(name) == false) {
 		// TOGGLE FUNKER! Må bare endre indre verdi til riktig type error.
 	    //alert('Your search string contains illegal characters');
-	    //$('#collapse-username').getElementByTagName('p').innerHTML = "Error"
-	    $('#collapse-username').collapse('toggle')
+	    //$('#username-error').innerHTML = "Error"
+	    //console.log($('#username-error').innerHTML)
+	    document.getElementById("username-error").innerHTML = "Username contains illegal characters"
+	    $('#collapse-username').collapse('show')
+	    should_register = false
 	}
 	else if (name.length < 6 || name.length > 20) {
-		alert("Brukernavnet må være på mellom 6 og 20 tegn")
+	    document.getElementById("username-error").innerHTML = "Username must be between 6-20 characters"
+	    $('#collapse-username').collapse('show')
+		should_register = false
 	}
-	else if ((password.length) < 8 || name.length > 32) {
-		alert("Passord må være mellom 8-32 karakterer langt");
-	} 
-	else if (!(password).match(cpassword)) {
-		alert("Passord matcher ikke");
-	} 
+
 	else {
+	    $('#collapse-username').collapse('hide')
+	    document.getElementById("username-error").innerHTML = ""
+
+	}
+
+	if ((password.length) < 8 || name.length > 32) {
+	    document.getElementById("password-error").innerHTML = "Password must be between 8-32 characters"
+	    $('#collapse-password').collapse('show')
+		should_register = false
+	}
+
+	else{
+	    $('#collapse-password').collapse('hide')
+	    document.getElementById("password-error").innerHTML = ""
+
+	}
+	if (!(password).match(cpassword)) {
+	    document.getElementById("cpassword-error").innerHTML = "Passwords doesn't match"
+	    
+	    $('#collapse-cpassword').collapse('show')
+		should_register = false
+	}
+
+	else{
+	    $('#collapse-cpassword').collapse('hide')
+	    document.getElementById("cpassword-error").innerHTML = ""
+
+	}
+
+
+	if(should_register) {
 		$.get('/accounts/register/', {username: $('#username').val(), email: $('#email').val(), password: $('#password').val()},
   		function(data){
 
@@ -34,6 +72,5 @@ function register() {
                 console.log(data);
    		});
 	}
-
 }
 $('#register-submit').click(function (){register()});
