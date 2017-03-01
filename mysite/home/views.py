cd87dddb183d4a0f8679ded5807904ced3e40b1b
ludvigk <ludde.killingberg@gmail.com>
2017-03-01 10:30:54 +0100

    Added template for home views

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,4 +1,58 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
+
+@login_required(login_url='/accounts/login/')
 def home(request):
     return render(request, 'home/base.html')
+
+
+@login_required(login_url='/accounts/login/')
+def subject(request):
+    return HttpResponse('')
+
+
+@login_required(login_url='/accounts/login/')
+def add_subject(request):
+    return HttpResponse('')
+
+
+@login_required(login_url='/accounts/login/')
+def create_subject(request):
+    return HttpResponse('')
+
+
+@login_required(login_url='/accounts/login/')
+def rate_lecture(request):
+    return HttpResponse('')
+
+
+@login_required(login_url='/accounts/login/')
+def rate_tag(request):
+    return HttpResponse('')
+
+
+@login_required(login_url='/accounts/login/')
+def add_tag(request):
+    return HttpResponse('')
+
+
+@login_required(login_url='/accounts/login/')
+def remove_tag(request):
+    return HttpResponse('')
+
+
+@login_required(login_url='/accounts/login/')
+def add_lecture(request):
+    return HttpResponse('')
+
+
+@login_required(login_url='/accounts/login/')
+def edit_lecture(request):
+    return HttpResponse('')
+
+
+@login_required(login_url='/accounts/login/')
+def remove_lecture(request):
+    return HttpResponse('')
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,9 +1,5 @@
-from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.http import HttpResponse
 from django.shortcuts import redirect
-from django.template import RequestContext
 
 
 @login_required(login_url='/accounts/login/')
