1d1a3ef87a4284eeb9440a9b03520ae943debaef
ludvigk <ludde.killingberg@gmail.com>
2017-03-01 11:21:53 +0100

    Fixed subject tags

--- mysite/accounts/models.py
+++ mysite/accounts/models.py
@@ -6,8 +6,6 @@ class Subject(models.Model):
     title = models.CharField(max_length=200)
     subjectCode = models.CharField(max_length=10)
     created_date = models.DateTimeField(default=timezone.now)
-    admins = models.ManyToManyField('auth.User', through='SubjectAdmin')
-    tags = models.ManyToManyField('Tag', through='SubjectTags')
 
     def __str__(self):
         return self.title
@@ -16,22 +14,23 @@ class Subject(models.Model):
         ordering = ('created_date', )
 
 
-class SubjectAdmin(models.Model):
-    admin = models.ForeignKey('auth.User')
+class SubjectUser(models.Model):
+    user = models.ForeignKey('auth.User')
     subject = models.ForeignKey('Subject')
+    permissions = models.CharField(max_length=20)
 
 
 class Tag(models.Model):
     creator = models.ForeignKey('auth.User')
     title = models.CharField(max_length=20)
-
-
-class SubjectTags(models.Model):
-    tag = models.ForeignKey(Tag)
-    subject = models.ForeignKey('Subject', on_delete=models.CASCADE)
+    subject = models.ForeignKey('Subject')
 
 
 class Lecture(models.Model):
     subject = models.ForeignKey('Subject', on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     date = models.DateTimeField()
+
+class Profile(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    subjects = models.ManyToManyField('Subject', through='SubjectUser')
